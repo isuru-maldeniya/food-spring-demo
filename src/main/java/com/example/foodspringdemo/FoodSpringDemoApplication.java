084982e93019e4c30877c3420363eb36473e7afc
Isuru Maldeniya <isurumaldeniya@gmail.com>
2022-08-20 20:39:01 +0530

    initial commit

---
+++ src/main/java/com/example/foodspringdemo/FoodSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodSpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodspringdemo/FoodSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
